61787669c4ffb78888130553f2a695939f386ecc
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-05-23 16:47:20 -0400

    First Class Functions

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback) {
+    return callback ()
+}
+function returnsANamedFunction(){
+return function kurstyn(){
+}
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+    }
+}
